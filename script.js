acaacb77b30a5398efc3cbe5ac37f53d53482602
Md Sajid <sajidcse202024@gmail.com>
2026-06-30 17:55:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function volume_sphere() {
     //Write your code here
+	const radius = parseFloat(document.getElementById("radius").value);
+    const volume = document.getElementById("volume");
+
+    if (isNaN(radius) || radius < 0) {
+        volume.value = "NaN";
+        return false;
+    }
+
+    const result = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    volume.value = result.toFixed(4);
+
+    return false;
   
 } 
 
